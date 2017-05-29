45b7384d293c4f48bae5771080a745150f934b2e
nikki93 <s.nikhilesh@gmail.com>
2017-05-28 22:12:10 -0700

    blank expo project

---
+++ main.js
@@ -0,0 +1,24 @@
+import Expo from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>hello, world</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+Expo.registerRootComponent(App);
