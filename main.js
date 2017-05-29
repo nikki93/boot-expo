69dddc3bce78cdc6e9ec936aeed0d552246965fd
nikki93 <s.nikhilesh@gmail.com>
2017-05-28 22:36:01 -0700

    try to use cljs build output, fails

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+require('./target/main.js')
+
 class App extends React.Component {
   render() {
     return (
