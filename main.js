6be777e8e83f140751ed46f360b5634a3d0d0e5b
nikki93 <s.nikhilesh@gmail.com>
2017-05-29 00:43:03 -0700

    basic cljs dev setup initialization, works

--- main.js
+++ main.js
@@ -2,13 +2,60 @@ import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-require('./target/main.js')
+const packagerHost = Expo.Constants.manifest.bundleUrl
+                         .match(/^https?:\/\/.*?\//)[0];
+
+const loadTargetAsync = async (path, success, error) => {
+  console.log('loading', path);
+  try {
+    const response = await fetch(
+      `${packagerHost}target/main.out/${path}`);
+    const text = await response.text();
+    (0, eval)(text);
+    success && success();
+  } catch (e) {
+    error && error();
+    console.log(`Error loading target '${path}':`, e.message);
+  }
+}
+
+const initCLJSDevAsync = async () => {
+  // Load and configure Google Closure Library for Expo context
+  await loadTargetAsync('goog/base.js');
+  goog.basePath = 'goog/';
+  goog.writeScriptSrcNode = loadTargetAsync;
+  goog.global.CLOSURE_IMPORT_SCRIPT = (src) =>
+    (loadTargetAsync(src), true);
+
+  // Normal CLJS startup process
+  loadTargetAsync('cljs_deps.js');
+  await loadTargetAsync('goog/deps.js');
+  await loadTargetAsync('../main.js');
+}
+
 
 class App extends React.Component {
+  state = {
+    initialized: false,
+  }
+
+  async componentDidMount() {
+    await initCLJSDevAsync();
+    this.setState({ initialized: true });
+  }
+
   render() {
-    return (
+    return this.state.initialized ? (
       <View style={styles.container}>
         <Text>hello, world</Text>
+      </View>
+    ) : (
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <Text>initializing cljs dev setup...</Text>
       </View>
     );
   }
